e4bb64c0535c9f73d836c9bb2598434f5cf2050c
Dipta04 <diptakarmakar38@gmail.com>
2022-09-27 16:12:19 +0600

    finished

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,12 +4,14 @@ import "./Cart.css"
 const Cart = (props) => {
 
     const {cart}=props;
-    console.log(cart);
+    // console.log(cart);
 
    let total=0;
    let shipping=0;
+   let quantity= 0;
    for(const product of cart){
-    total=total+product.price;
+    quantity = quantity+product.quantity;
+    total=total+product.price * product.quantity;
     shipping=shipping+product.shipping;
    }
 
@@ -20,7 +22,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
           <h4>order summary</h4>
-        <p>Selected Items: {cart.length}</p>  
+        <p>Selected Items: {quantity}</p>  
         <p>Total price: ${total}</p>
         <p>Total Shipping: ${shipping} </p>
         <p>Tax: {tax}</p>
