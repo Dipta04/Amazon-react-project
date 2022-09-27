212e32bb132b073949df2880091a763ae13cf8df
Dipta04 <diptakarmakar38@gmail.com>
2022-09-27 08:06:11 +0600

    first commit

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import "./Shop.css"
+
+const Shop = () => {
+    // product ar jonno ata
+    const [products,setProducts] = useState([]);
+
+// cart ar jonno ata
+    const [cart,setCart]=useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    }, [])
+
+    // cart button (akhan theka product js a)
+    const handleAddToCart = (product) =>{
+        console.log(product);
+        const newCart= [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+
+           <div className='product-container'>
+     {
+        products.map(product=> <Product 
+            key={product.id}
+            product={product}
+            handleAddToCart ={handleAddToCart}
+            ></Product>)
+     }
+           </div>
+
+           <div className='cart-container'>
+            <h4>order summary</h4>
+            <p>Selected Items: {cart.length}</p>
+           </div>
+
+        </div>
+    );
+};
+
+export default Shop;
